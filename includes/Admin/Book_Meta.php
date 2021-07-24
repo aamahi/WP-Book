57191663d3f696462e172ae89d7e25ed3dca8a45
amahi <amahi.wp@gmail.com>
2021-07-24 17:16:09 +0600

    Book meta data sanitaize and save

--- includes/Admin/Book_Meta.php
+++ includes/Admin/Book_Meta.php
@@ -15,6 +15,70 @@ class Book_Meta {
      */
     public function __construct() {
         add_action( 'add_meta_boxes', [ $this, 'add_book_meta_box' ], 10, 2 );
+        add_action( 'save_post', [ $this, 'save_book_meta_data' ] );
+    }
+
+    /**
+     * check secure meta box field
+     *
+     * @param $nonce_field
+     * @param $action
+     * @param $post_id
+     *
+     * @return bool
+     */
+    private function is_secured( $nonce_field, $action, $post_id  ) {
+
+        $nonce = isset( $_POST[ $nonce_field ] ) ? $_POST[ $nonce_field ] : "" ;
+
+        if ( $nonce == '' ) {
+            return false;
+        }
+        if( ! wp_verify_nonce( $nonce, $action ) ) {
+            return false;
+        }
+
+        if ( wp_is_post_autosave( $post_id ) ) {
+            return false;
+        }
+
+        if ( wp_is_post_revision( $post_id ) ) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * save all book meta data.
+     *
+     * @param $post_id
+     *
+     * @return mixed
+     */
+    public function save_book_meta_data( $post_id ) {
+
+        /**
+         * check security
+         */
+        if ( ! $this->is_secured( 'mahi_wp_book_field', 'mahi_wp_book', $post_id ) ) {
+            return $post_id;
+        }
+
+        $book_author    = isset( $_POST['book_author'] ) ? sanitize_text_field( wp_unslash( $_POST['book_author'] ) ) : '';
+        $book_price     = isset( $_POST['book_price'] ) ? sanitize_text_field( wp_unslash( $_POST['book_price'] ) ) : '';
+        $book_publisher = isset( $_POST['book_publisher'] ) ? sanitize_text_field( wp_unslash( $_POST['book_publisher'] ) ) : '';
+        $book_year      = isset( $_POST['book_year'] ) ? sanitize_text_field( wp_unslash( $_POST['book_year'] ) ) : '';
+        $book_edition   = isset( $_POST['book_edition'] ) ? sanitize_text_field( wp_unslash( $_POST['book_edition'] ) ) : '';
+        $book_url       = isset( $_POST['book_url'] ) ? sanitize_trackback_urls( wp_unslash( $_POST['book_url'] ) ) : '';
+
+        update_post_meta( $post_id, 'book_author', $book_author );
+        update_post_meta( $post_id, 'book_price', $book_price );
+        update_post_meta( $post_id, 'book_publisher', $book_publisher );
+        update_post_meta( $post_id, 'book_year', $book_year );
+        update_post_meta( $post_id, 'book_edition', $book_edition );
+        update_post_meta( $post_id, 'book_url', $book_url );
     }
 
     /**
@@ -32,14 +96,34 @@ class Book_Meta {
     }
 
     /**
-     * display Book meta box.
+     * Display book meta box
+     *
+     * @param $post
      */
-    public function display_book_meta_boxes() {
+    public function display_book_meta_boxes( $post ) {
         /**
          * load subscription-metadata-style file
          */
         wp_enqueue_style( 'book_meta_data_style' );
 
+        /**
+         * wp_nonce field.
+         */
+        wp_nonce_field( 'mahi_wp_book', 'mahi_wp_book_field' );
+
+        /**
+         * Get all Book meta data.
+         */
+        $book_author    = get_post_meta( $post->ID, 'book_author', true );
+        $book_price     = get_post_meta( $post->ID, 'book_price', true );
+        $book_publisher = get_post_meta( $post->ID, 'book_publisher', true );
+        $book_year      = get_post_meta( $post->ID, 'book_year', true );
+        $book_edition   = get_post_meta( $post->ID, 'book_edition', true );
+        $book_url       = get_post_meta( $post->ID, 'book_url', true );
+
+        /**
+         * load wp book meta data form
+         */
         ob_start();
         include "view/book_meta_data.php";
     }
--- includes/Admin/view/book_meta_data.php
+++ includes/Admin/view/book_meta_data.php
@@ -5,45 +5,42 @@
             <label for="author"> <?php _e( "Author Name", 'wp-book' );  ?> </label>
         </div>
         <div class="col-75">
-            <input type="text" id="author" name="book_author" value="" placeholder="Book Author Name">
+            <input type="text" id="author" name="book_author" value="<?php if( isset( $book_author ) ) { echo esc_attr( $book_author ); } ?>" placeholder="Book Author Name">
         </div>
 
         <div class="col-25">
             <label for="price"> <?php _e( "Price", 'wp-book' );  ?> </label>
         </div>
         <div class="col-75">
-            <input type="number" id="price" name="book_price" value="" placeholder="Price">
+            <input type="number" id="price" name="book_price" value="<?php if( isset( $book_price ) ) { echo esc_attr( $book_price ); } ?>" placeholder="Price">
         </div>
 
         <div class="col-25">
             <label for="publisher"> <?php _e( "Book Publisher", 'wp-book' );  ?> </label>
         </div>
         <div class="col-75">
-            <input type="text" id="publisher" name="book_publisher" value="" placeholder="Book Publisher">
+            <input type="text" id="publisher" name="book_publisher" value="<?php if( isset( $book_publisher ) ) { echo esc_attr( $book_publisher ); } ?>" placeholder="Book Publisher">
         </div>
 
         <div class="col-25">
             <label for="year"> <?php _e( "Year", 'wp-book' );  ?> </label>
         </div>
         <div class="col-75">
-            <input type="number" id="year" name="book_year" value="" placeholder="Year">
+            <input type="number" id="year" name="book_year" value="<?php if( isset( $book_year ) ) { echo esc_attr( $book_year ); } ?>" placeholder="Year">
         </div>
 
         <div class="col-25">
             <label for="edition"> <?php _e( "Edition", 'wp-book' );  ?> </label>
         </div>
         <div class="col-75">
-            <input type="text" id="edition" name="book_edition" value="" placeholder="Book Edition">
+            <input type="text" id="edition" name="book_edition" value="<?php if( isset( $book_edition ) ) { echo esc_attr( $book_edition ); } ?>" placeholder="Book Edition">
         </div>
 
         <div class="col-25">
             <label for="url"> <?php _e( "URL", 'wp-book' );  ?> </label>
         </div>
         <div class="col-75">
-            <input type="number" id="url" name="book_url" value="" placeholder="Book URL">
+            <input type=text id="url" name="book_url" value="<?php if( isset( $book_url ) ) { echo esc_attr( $book_url ); } ?>" placeholder="Book URL">
         </div>
     </div>
 </div>
-
-
-<?php //if( isset( $subscription_price ) ) { echo esc_attr( $subscription_price ); } ?>
